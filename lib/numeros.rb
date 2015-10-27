1bd5a022503592628de2b486de710a367d812449
Kevin <alu0100789461@ull.edu.es>
2015-10-27 20:45:56 +0000

    Añadida y funcionando la funcion de maximo comun divisor, tambien puesta su correspondiente prueba

--- lib/numeros.rb
+++ lib/numeros.rb
@@ -4,9 +4,25 @@ class Numero
     def initialize(x, y)
         raise RuntimeError, 'El denominador no puede ser 0' if y == 0 #Controlamos que el denominador no sea cero
         @x, @y = x, y
+        
+        mcd(@x,@y) #Llamamos a la funcion para simplificar la fraccion introducida
     end
     
-    def to_s
+    def to_s # Salida formateada
         "#{@x}/#{@y}"
     end
+    
+    def mcd(x,y) #Maximo comun divisor
+        x, y = x.abs, y.abs
+        while y != 0
+            x, y = y, x % y
+        end
+        x
+        @x = @x/x
+        @y = @y/x
+    end
+    
+    #def + (other)
+        
+    #end
 end
--- test/tc_numeros.rb
+++ test/tc_numeros.rb
@@ -4,5 +4,6 @@ require "test/unit"
 class TestNumeros < Test::Unit::TestCase
     def test_init
         assert_equal("3/2", Numero.new(3,2).to_s)
+        assert_equal("59/19", Numero.new(177,57).to_s)
     end
 end
