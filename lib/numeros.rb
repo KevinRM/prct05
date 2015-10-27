1dbafd15a633ad678bfeefe397aa277b8535877b
Kevin <alu0100789461@ull.edu.es>
2015-10-27 21:10:11 +0000

    Añadida operacion producto con su correspondiente test

--- lib/numeros.rb
+++ lib/numeros.rb
@@ -33,4 +33,10 @@ class Numero
         @y = (@y*other.y)
         mcd(@x,@y)
     end
+    
+    def * (other) # Producto
+        @x = (@x*other.x)
+        @y = (@y*other.y)
+        mcd(@x,@y)
+    end
 end
--- test/tc_numeros.rb
+++ test/tc_numeros.rb
@@ -2,7 +2,7 @@ require "lib/numeros"
 require "test/unit"
 
 class TestNumeros < Test::Unit::TestCase
-    def test_init
+    def test_inicializacion
         assert_equal("3/2", Numero.new(3,2).to_s)
         assert_equal("59/19", Numero.new(177,57).to_s)
     end
@@ -17,5 +17,11 @@ class TestNumeros < Test::Unit::TestCase
         n2 = Numero.new(2,3)
         n1 - n2
         assert_equal("1/6", n1.to_s)
+    end
+    def test_producto
+        n1 = Numero.new(4,5)
+        n2 = Numero.new(5,3)
+        n1 * n2
+        assert_equal("4/3", n1.to_s)
     end
 end
